11b70d8a3799ee5d34be0008411d16cdd1efff1d
Abhishek Kemana <93156114+Ak030702@users.noreply.github.com>
2025-10-29 11:35:07 +0530

    Fix Docker build commands syntax

--- vars/dockerbuild.groovy
+++ vars/dockerbuild.groovy
@@ -1,4 +1,4 @@
 def call(String frontend_path,String backend_path)
   echo "Building Docker Container"
-  sh "docker build -t backend ./"${frontend_path}""
-  sh "docker build -t frontend ./"${backend_path}""
+  sh """docker build -t backend ./${frontend_path}"""
+  sh """docker build -t frontend ./${backend_path}"""
